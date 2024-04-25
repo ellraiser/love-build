5e70113e9563c65302d507a2c79f54551adc6e09
ell <77150506+ellraiser@users.noreply.github.com>
2024-04-25 12:13:00 +0100

    Update build.lua

--- example-project/build.lua
+++ example-project/build.lua
@@ -3,7 +3,7 @@ return {
   developer = 'ellraiser',
   output = 'dist',
   version = '1.0.0',
-  love = '12.0',
+  love = '11.5',
   ignore = {'dist', '.DS_Store'},
   libs = {'resources/plugin.dll'},
   icon = 'resources/icon.png',
