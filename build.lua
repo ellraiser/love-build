c2c6d199cf8ce2c65a3df608a2445ec80407e638
ell <77150506+ellraiser@users.noreply.github.com>
2024-04-25 22:43:29 +0100

    update main.yaml

--- build.lua
+++ build.lua
@@ -6,6 +6,7 @@ return {
   icon = 'resources/love-hammer.png',
   identifier = 'com.love.build',
   ignore = {
-    'dist', 'example-project', 'love12', 'love12.zip', 'love-macos'
+    'dist', 'example-project',
+    'love12', 'love12.zip', 'love-macos', 'love-build.lua' -- github workflow
   }
 }
